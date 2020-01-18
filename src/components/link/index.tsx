a16cfadb54ef80ef1d368b256903737be2544bf6
Hank Andre <hankandre@gmail.com>
2020-01-17 23:47:19 -0600

    chore(deps): remove gatsby-plugin-transition-link for the starter

--- src/components/link/index.tsx
+++ src/components/link/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import AniLink from 'gatsby-plugin-transition-link/AniLink';
+import { Link as GatsbyLink } from 'gatsby';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function Link({ href, ...props }: any) {
@@ -7,7 +7,7 @@ export default function Link({ href, ...props }: any) {
 
   return internal ? (
     // eslint-disable-next-line react/jsx-props-no-spreading
-    <AniLink fade to={href} {...props} />
+    <GatsbyLink to={href} {...props} />
   ) : (
     // eslint-disable-next-line jsx-a11y/anchor-has-content,react/jsx-props-no-spreading
     <a href={href} {...props} />
--- src/components/link/link.test.js
+++ src/components/link/link.test.js
@@ -3,6 +3,7 @@ import { render } from '@testing-library/react';
 import Link from '.';
 
 test('<Link/>', () => {
+  window.matchMedia = jest.fn(() => true);
   const href = '/test-link';
   const { container } = render(<Link href={href}>Hello</Link>);
   expect(container.firstChild)
